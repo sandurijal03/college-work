1a66cfb44e32178940b77569cfec43da5c001158
Sandip Rijal <sandurijal03@hotmail.com>
2021-04-19 11:21:40 +0545

    implementing styled components in car page

--- client/src/components/Car/CarPage.js
+++ client/src/components/Car/CarPage.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
+import styled from 'styled-components';
 
 import { GET_CAR } from '../../queries';
-
-import styled from 'styled-components';
 
 const CarPage = ({ match }) => {
   const { _id } = match.params;
